e5f446de5523678a3c2998184868ceef3b388696
pauloacf <pauloacf148@gmail.com>
2020-03-10 00:47:02 -0400

    Tentativa

--- desafio.c
+++ desafio.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
+
+typedef struct livro {
+   char nome[1000], autor[1000];
+   long long int isbn;
+   float preco; 
+} livro;
+
+void inserir_livro(livro *nome, livro *autor, livro *isbn, livro *preco){
+    FILE *file;
+    fopen("livros.txt", "a");
+    fprintf(file, nome, "\n");
+    fprintf(file, autor, "\n");
+    fprintf(file, isbn, "\n");
+    fprintf(file, preco, "\n");
+    fclose(file);
+}
 
 int main(){
-
     int option = 0;
     bool running = true;
     printf("        Menu\n 1 - Inserir livro.\n 2 - Remover livro.\n 3 - Procurar livro.\n 4 - Sair.\n");
@@ -13,6 +30,14 @@ int main(){
 
         switch (option){
         case 1:
+            livro teste;
+            teste.nome;
+            teste.autor;
+            strcpy(teste.nome, "Fundamentos de Sistemas Operacionais");
+            strcpy(teste.autor, "Silberschatz");
+            teste.isbn = 9788521617471;
+            teste.preco = 250.0;
+            inserir_livro(teste.nome, teste.autor, teste.isbn, teste.preco);
             printf("1\n");
             break;
         
